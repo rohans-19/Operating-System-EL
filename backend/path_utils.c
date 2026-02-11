b6a0c4f26455226af0c63808d4f8bf2e3085df63
Rohan S <rohanshreedhar19@gmail.com>
2026-02-11 23:40:52 +0530

    path uitls updated

--- backend/path_utils.c
+++ backend/path_utils.c
@@ -2,16 +2,35 @@
 #include <string.h>
 
 int is_sensitive(const char *p) {
+    
     return strstr(p, "/etc/passwd") ||
            strstr(p, "/etc/shadow") ||
-           strstr(p, "/proc") ||
-           strstr(p, "/sys")||
-           strstr(p, "/etc/shadow-") ||
-           strstr(p, "/etc/gshadow") ||
+           strstr(p, "/etc/shadow-") ||      
+           strstr(p, "/etc/gshadow") ||      
+           
+           
            strstr(p, "/etc/sudoers") ||
+           strstr(p, "/etc/sudoers.d/") ||
+           
+           
            strstr(p, "/etc/ssh/") ||
-           strstr(p, "/boot/") ||
-           strstr(p, "/dev/mem")||
-           strstr(p, "/dev/kmem");
+           strstr(p, "/.ssh/") ||             
+           strstr(p, "/root/.ssh/") ||
+           
+           
+           strstr(p, "/proc") ||
+           strstr(p, "/sys") ||
+           strstr(p, "/boot") ||
+           
+           
+           strstr(p, "/dev/mem") ||
+           strstr(p, "/dev/kmem") ||
+           strstr(p, "/dev/port") ||
+           
+           
+           strstr(p, "/etc/security/") ||    
+           strstr(p, "/etc/pam.d/") ||       
+           strstr(p, "/etc/group") ||        
+           strstr(p, "/etc/hosts.allow") ||  
+           strstr(p, "/etc/hosts.deny");
 }
-
